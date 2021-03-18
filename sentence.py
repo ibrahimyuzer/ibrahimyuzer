c14cc3abaaf66eadfbecceb0171c90be1c15d61b
ibrahimyuzer <ibrahimyuzer@users.noreply.github.com>
2021-03-18 11:42:03 +0300

    asasa

---
+++ sentence.py
@@ -0,0 +1,30 @@
+sentence = " I want to play a game     "
+# sentence = sentence.upper()
+# print(sentence)
+# sentence = sentence.lower()
+# print(sentence)
+# sentence = sentence.title()
+# print(sentence)
+# sentence = sentence.capitalize()
+# print(sentence)
+
+# sentence = sentence.split()
+# sentence = '.'.join(sentence)
+# print(sentence)
+
+# sorgu = sentence.find('want')
+# print(sorgu)
+
+# varmi = sentence.startswith('I')
+# print(varmi)
+# varmi2 = sentence.endswith('r')
+# print(varmi2)
+
+# sentence = sentence.replace('want','like').replace("game","playstation")
+# print(sentence)
+
+# sentence = sentence.center(50,"#")
+# print(sentence)
+
+sentence = sentence.rstrip()
+print(sentence)
