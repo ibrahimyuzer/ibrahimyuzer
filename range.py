7da127e9c9ce8dc4b45a45ed170c395af259c59d
ibrahimyuzer <ibrahimyuzer@users.noreply.github.com>
2021-03-18 11:42:45 +0300

    raghm

---
+++ range.py
@@ -0,0 +1,19 @@
+# liste = [1,2,3,4,5]
+# for i in liste:
+#     print(i)
+
+liste = range(1,100,2)
+print(*liste)
+
+liste1 = range(100,1,-3)
+print(*liste1)
+
+
+for i in range(1,200,7):
+    print(i)
+for i in range(1,200,7):
+    print(i*2)
+
+a = " ibrahim"
+for i in range(1,10):
+    print(i * a)
