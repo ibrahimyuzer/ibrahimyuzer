28e8adb80cf87ae46e8125183ac70a821668fb1f
ibrahimyuzer <ibrahimyuzer@users.noreply.github.com>
2021-03-18 11:42:28 +0300

    asqwqwq

---
+++ continue.py
@@ -0,0 +1,18 @@
+# for i in range(1,40):
+#     if (i < 30):
+#         continue
+#     print(i)
+
+# for i in range(1,100):
+#     if (i > 30 and i < 80):
+#         continue
+#     print(i)
+
+while True:
+    i = 0
+    if (i == 100):
+        break
+    for i in range(1,101):
+        if (i > 50 and i < 80):
+            continue
+    print(i)
